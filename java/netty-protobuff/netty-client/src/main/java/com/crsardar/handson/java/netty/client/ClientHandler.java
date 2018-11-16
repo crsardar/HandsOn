3fbf152d2c46313a776e61593a295d09758a7a61
Chittaranjan Sardar <Chittaranjan.Sardar@Icumed.com>
2018-11-16 21:01:27 +0530

    Hands On - Simple Netty with Proto-Buff

---
+++ java/netty-protobuff/netty-client/src/main/java/com/crsardar/handson/java/netty/client/ClientHandler.java
@@ -0,0 +1,52 @@
+package com.crsardar.handson.java.netty.client;
+
+import com.crsardar.handson.java.protobuff.client.ClientMsgContainer;
+import com.crsardar.handson.java.protobuff.server.ServerResponseContainer;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class ClientHandler extends SimpleChannelInboundHandler<ServerResponseContainer.ServerResponse> {
+
+    @Override
+    public void channelActive(final ChannelHandlerContext ctx) throws Exception {
+
+        System.out.println("Client : channelActive() : Thread = " + Thread.currentThread().getId());
+
+        // Because All Calls On SAME Thread
+        new Thread(() -> {
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Please type a message and Enter - ");
+
+            while (true) {
+
+                String msg = scanner.nextLine();
+
+                ClientMsgContainer.ClientMsg.Builder msgBuilder = ClientMsgContainer.ClientMsg.newBuilder();
+                msgBuilder.setClientName("MyNettyClient");
+                msgBuilder.setTime(LocalDateTime.now().toString());
+                msgBuilder.setMessage(msg);
+
+                ClientMsgContainer.ClientMsg clientMsg = msgBuilder.build();
+
+                ctx.writeAndFlush(clientMsg);
+            }
+        }).start();
+
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, ServerResponseContainer.ServerResponse serverResponse) throws Exception {
+
+        System.out.println("Client : channelRead0() : Thread = " + Thread.currentThread().getId());
+
+        System.out.println("===> Client Receives : " + serverResponse.toString());
+    }
+}
