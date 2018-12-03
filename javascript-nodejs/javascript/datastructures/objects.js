1d0f8b096be84b6fa806a8e9d2c974367b68ae59
Chittaranjan Sardar <Chittaranjan.Sardar@Icumed.com>
2018-12-03 21:06:57 +0530

    HandsOn - JavaScript - Arrays, Objects, Methods

---
+++ javascript-nodejs/javascript/datastructures/objects.js
@@ -0,0 +1,21 @@
+
+let myObject = {
+
+    firstName : 'Hari',
+    lastName : 'Ohm',
+    attributes : ['Creator', 'Maitainer', 'Destructor']
+}
+
+console.log('First Name = ' + myObject.firstName);
+console.log('Has LastName = ' + ('lastName' in myObject));
+console.log('attributes = ' + myObject['attributes']);
+console.log('Yetnotdefined = ' + myObject.capabiliy);
+myObject.capabiliy = 'Beyond Imagination';
+console.log('Capability = ' + myObject.capabiliy);
+console.log('All attributes = ' + Object.keys(myObject));
+
+
+let myObject2 = {canNotDo:'Yet not defined!'};
+Object.assign(myObject2, myObject);
+
+console.log(myObject2);
