a1ba37c19ffec3e673ed57577958dba76cf4cc6c
Chittaranjan Sardar <crsardar.etc@gmail.com>
2019-01-13 16:01:25 +0530

    HandsOn - Android, JaCoco code coverage basic

--- javascript-nodejs/javascript/datastructures/objects.js
+++ javascript-nodejs/javascript/datastructures/objects.js
@@ -1,21 +1,35 @@
 
-let myObject = {
+// let myObject = {
 
-    firstName : 'Hari',
-    lastName : 'Ohm',
-    attributes : ['Creator', 'Maitainer', 'Destructor']
+//     firstName : 'Hari',
+//     lastName : 'Ohm',
+//     attributes : ['Creator', 'Maitainer', 'Destructor']
+// }
+
+// console.log('First Name = ' + myObject.firstName);
+// console.log('Has LastName = ' + ('lastName' in myObject));
+// console.log('attributes = ' + myObject['attributes']);
+// console.log('Yetnotdefined = ' + myObject.capabiliy);
+// myObject.capabiliy = 'Beyond Imagination';
+// console.log('Capability = ' + myObject.capabiliy);
+// console.log('All attributes = ' + Object.keys(myObject));
+
+
+// let myObject2 = {canNotDo:'Yet not defined!'};
+// Object.assign(myObject2, myObject);
+
+// console.log(myObject2);
+
+
+let objOne = {
+    name : 'Chittaranjan',
+    qualifications : []
 }
 
-console.log('First Name = ' + myObject.firstName);
-console.log('Has LastName = ' + ('lastName' in myObject));
-console.log('attributes = ' + myObject['attributes']);
-console.log('Yetnotdefined = ' + myObject.capabiliy);
-myObject.capabiliy = 'Beyond Imagination';
-console.log('Capability = ' + myObject.capabiliy);
-console.log('All attributes = ' + Object.keys(myObject));
+objOne.console = ['Java', 'Algo', 'Design Patterns']
 
+objOne.console.forEach(element => {
+    console.log(element)
+});
 
-let myObject2 = {canNotDo:'Yet not defined!'};
-Object.assign(myObject2, myObject);
-
-console.log(myObject2);
+console.log("Length = " + objOne.qualifications.length) // 
