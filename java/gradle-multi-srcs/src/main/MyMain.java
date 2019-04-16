6e15c9ea697efc27e29b56f0b39aa32103c586a9
Chittaranjan Sardar <crsardar@gmail.com>
2019-04-16 07:11:11 +0530

    HandsOn - backup

--- java/gradle-multi-srcs/src/main/MyMain.java
+++ java/gradle-multi-srcs/src/main/MyMain.java
@@ -1,7 +1,5 @@
-public class MyMain
-{
-    public static void main(String... arg)
-    {
+public class MyMain {
+    public static void main(String... arg) {
         System.out.println(new ExtraClass().giveDouble(9));
     }
 }
