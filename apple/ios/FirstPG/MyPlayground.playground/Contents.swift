9b9209c1c9a0bb18fb4665cd04bf8ccd2d3701df
Chittaranjan Sardar <crsardar@gmail.com>
2019-06-02 20:20:18 +0530

    HandsOn : - iOS

---
+++ apple/ios/FirstPG/MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+print("Hello World!")
+
+var number1:Int64 = 1
+//let result:Int64 = number1 + 10 - This is wrong, constants does NOT need type modifiers
+let result = number1 + 12
+print("My result = \(result)")
+
+var int = 1;
+int += 1
+print("My  Int = \(int)")
+
+var double = 10 % 3
+print("My double = \(double)")
+
+var name = "Chittaranjan"
+print("My name is \(name)")
+name = 113
+pr
+
